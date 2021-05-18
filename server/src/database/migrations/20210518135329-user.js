19fa9a0854ff94080b2f1cf1f0d240825fc80f7b
Philippe De Pauw - Waterschoot <drdynscript>
2021-05-18 18:00:58 +0200

    Added migrations to database so it can generate a scheme

---
+++ server/src/database/migrations/20210518135329-user.js
@@ -0,0 +1,15 @@
+
+import 'babel-polyfill';
+
+import database from '../index';
+database.connect();
+
+export default {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.createTable(database.User.tableName, database.User.attributes);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.dropTable(database.User.tableName);
+  }
+};
